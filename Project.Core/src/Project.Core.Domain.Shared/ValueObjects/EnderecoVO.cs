dde6d13d11893b0030a04f703b70a05dda73b6ba
Rodrigo Santos da Soledade Gonçalves <rodrigo.goncalves@iterative.com.br>
2019-02-12 08:28:50 -0200

    Validações das entidades

--- Project.Core/src/Project.Core.Domain.Shared/ValueObjects/EnderecoVO.cs
+++ Project.Core/src/Project.Core.Domain.Shared/ValueObjects/EnderecoVO.cs
@@ -2,6 +2,12 @@
 {
     public class EnderecoVO
     {
+        public EnderecoVO()
+        {
+            UF = new UfVO();
+            CEP = new CepVO();
+        }
+
         public string Logradouro { get; set; }
         public string Bairro { get; set; }
         public string Cidade { get; set; }
