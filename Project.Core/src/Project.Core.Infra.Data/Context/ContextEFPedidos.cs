a7106a02b7922fe17df642e329125fab60f30a14
Rodrigo Santos da Soledade Gonçalves <rodrigo.goncalves@iterative.com.br>
2019-02-13 15:01:46 -0200

    Context de Pedidos

---
+++ Project.Core/src/Project.Core.Infra.Data/Context/ContextEFPedidos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Project.Core.Domain.Pedidos.Entidades;
+using System.IO;
+
+namespace Project.Core.Infra.Data.Context
+{
+    public class ContextEFPedidos : DbContext
+    {
+        public DbSet<Produtos> Produtos { get; set; }
+        public DbSet<Pedidos> Pedidos { get; set; }
+        public DbSet<ItensPedidos> ItensPedidos { get; set; }
+        public DbSet<Fornecedores> Fornecedores { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appSettings.json").Build();
+
+            dbContextOptionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
