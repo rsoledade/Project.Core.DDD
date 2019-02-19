cd95c13b561e6ed802401f056b59eec8c3e15838
Rodrigo Santos da Soledade Gonçalves <rodrigo.goncalves@iterative.com.br>
2019-02-19 11:07:50 -0300

    Front

---
+++ Project.Core/src/Project.Core.Presentation.FrontSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project.Core.Presentation.FrontSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
